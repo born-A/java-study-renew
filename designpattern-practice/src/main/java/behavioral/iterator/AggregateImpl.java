53d282c858f4428f0cb17c1007d775dd9da38a30
born-A <93516595+born-A@users.noreply.github.com>
2024-04-29 15:42:53 +0900

    design pattern - iterator

---
+++ designpattern-practice/src/main/java/behavioral/iterator/AggregateImpl.java
@@ -0,0 +1,33 @@
+package behavioral.iterator;
+
+public class AggregateImpl<E> implements Aggregate<E> {
+	
+	private E[] datas = null;
+	
+	public AggregateImpl(E[] datas) {
+		this.datas = datas;
+	}
+	
+	@Override
+	public Iterator<E> createIterator() {
+		return new IteratorImpl();
+	}
+	
+	private class IteratorImpl implements Iterator<E> {
+		int index = 0;
+
+		@Override
+		public E next() {
+			// TODO Auto-generated method stub
+			return index < datas.length ? datas[index++] : null;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return index < datas.length;
+		}
+		
+		
+	}
+
+}

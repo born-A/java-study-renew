aef8152958cfd8efb133406bdbd05b0e46a390cf
born-A <93516595+born-A@users.noreply.github.com>
2024-04-30 14:02:59 +0900

    io

---
+++ chapter04/src/main/java/io/PhoneList01.java
@@ -0,0 +1,26 @@
+package io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class PhoneList01 {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("./phone.txt");
+			if(file.exists()) {
+				System.out.println("file not found");
+				return;
+			}
+			
+			FileInputStream fis = new FileInputStream(file);
+			fis.read();
+		} catch (FileNotFoundException e) {
+			System.out.println();
+		} catch (IOException e) {
+			System.out.println();
+		}
+	}
+}

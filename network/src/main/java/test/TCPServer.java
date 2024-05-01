4b601d2c0bcaf9a2825365c555029b5a043d9ef2
born-A <93516595+born-A@users.noreply.github.com>
2024-05-01 16:53:57 +0900

    socket - client, server

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -14,16 +14,17 @@ public class TCPServer {
 		ServerSocket serverSocket = null;
 		
 		try {
-			//1. Server Socket 생성
+			// 1. Server Socket 생성
 			serverSocket = new ServerSocket();
 			
-			//2. 바인딩(binding)
-			//	Socket에 InetSocketAddress[InetAddress(IPAddress + Port)]를 바인딩한다.
-			// IPAddress : 0.0.0.0 : 특정 호스트 IP를 바인딩하지 않는다.
+			// 2. 바인딩(binding)
+			//    Socket에 InetSocketAddress[InetAddress(IPAddress) + Port]를 바인딩 한다.
+			//    IPAddress: 0.0.0.0: 특정 호스트 IP를 바인딩 하지 않는다.
 			serverSocket.bind(new InetSocketAddress("127.0.0.1", 5000), 10);
 			
-			// 3.accept
-			Socket socket = serverSocket.accept(); // blocking
+			// 3. accept
+			Socket socket = serverSocket.accept();  // blocking
+
 			
 			try {
 				InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
@@ -35,47 +36,51 @@ public class TCPServer {
 				InputStream is = socket.getInputStream();
 				OutputStream os = socket.getOutputStream();
 				
-				//5. 데이터 읽기
 				while(true) {
+					//5. 데이터 읽기
 					byte[] buffer = new byte[256];
-					int readByteCount = is.read(buffer); // blocking	
-					
+					int readByteCount = is.read(buffer); // blocking
 					if(readByteCount == -1) {
-						// 클라이언트가 정상적으로 종료 (close() 호출) 
+						// 클라이언트가 정상적으로 종료(close() 호출)
 						System.out.println("[server] closed by client");
 						break;
 					}
 					
 					String data = new String(buffer, 0, readByteCount, "UTF-8");
-					System.out.println("[server] received : " + data);
+					System.out.println("[server] received:" + data);
 					
 					// 6. 데이터 쓰기
 					os.write(data.getBytes("utf-8"));
+					
 				}
-				
 			} catch(SocketException e) {
-				System.out.println("[server] error : " + e);
+				System.out.println("[server] suddenly closed by client");
 			} catch(IOException e) {
-				System.out.println("[server] error : " + e);
+				System.out.println("[server] error:" + e);
 			} finally {
 				try {
-					if(serverSocket != null && !serverSocket.isClosed())
-						serverSocket.close();
-				} catch (IOException e) {
+					if(socket != null && !socket.isClosed()) {
+						socket.close();
+					}
+				} catch(IOException e) {
 					e.printStackTrace();
 				}
 			}
+
 			
 		} catch (IOException e) {
-			System.out.println("[server] error : " + e);
+			System.out.println("[server] error: " + e);
 		} finally {
 			try {
-				if(serverSocket != null && !serverSocket.isClosed())
+				if(serverSocket != null && !serverSocket.isClosed()) {
 					serverSocket.close();
+				}
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+
 	}
 
 }
